cadb5175d6091aec3bc48232fcbf34d2a61cb627
MegaChuck64 <cjscottcarey@yahoo.com>
2023-04-11 23:22:06 -0400

    updated wat2wasm annotated binary reference

--- CJWasm/Program.cs
+++ CJWasm/Program.cs
@@ -336,68 +336,75 @@ public enum ValueType
 
 
 /* 
-    var output = new List<byte>
-    {
-        //wasm magic number
-        0x0,0x61,   0x73,0x6D,
-        //version
-        0x01,0x00,  0x00,0x00,
-        
-        //new section | type - 5
-        //section type ID (1=type) and size 5-bytes | 'type' section contains function signatures
-        0x01,0x05,  
-        //number of types 
-        0x01,
-        //func
-        0x60,
-        //num params
-        0x00,
-        //num of results
-        0x01,
-        //result type
-        0x7f,
-
-        //new section | function - 2
-        //section type ID (3=function) and size 2-bytes | 'function' section contains indexes of the function signature
-        0x03, 0x02,
-        //number of functions
-        0x01,
-        //index of function
-        0x0,
-
-        //new section | export - 8
-        //section type ID (7=export) and size 8-bytes | 'export' section defines export name with the index to our function 
-        0x07, 0x08,
-        //number of exports
-        0x01,
-        //export name length
-        0x04,
-        //export name ("main")
-        0x6d, 0x61, 0x69, 0x6e,
-        //export type (0=function)
-        0x00,
-        //export index
-        0x00,
-
-        //new section | code - 10
-        //section type ID (10=code) and size 7-bytes | 'code' section contains the function body
-        0x0a, 0x07,
-        //number of functions
-        0x01,
-        //function body size
-        0x05,
-        //number of locals
-        0x00,
-
-        //function body
-        //load constant
-        0x41,
-        //constant value
-        0x2a,
-        //return
-        0x0f,
-        //end of function body
-        0x0b,
-
-    };
+0000000: 0061 736d                                 ; WASM_BINARY_MAGIC
+0000004: 0100 0000                                 ; WASM_BINARY_VERSION
+; section "Type" (1)
+0000008: 01                                        ; section code
+0000009: 00                                        ; section size (guess)
+000000a: 01                                        ; num types
+; func type 0
+000000b: 60                                        ; func
+000000c: 02                                        ; num params
+000000d: 7f                                        ; i32
+000000e: 7f                                        ; i32
+000000f: 01                                        ; num results
+0000010: 7f                                        ; i32
+0000009: 07                                        ; FIXUP section size
+; section "Function" (3)
+0000011: 03                                        ; section code
+0000012: 00                                        ; section size (guess)
+0000013: 01                                        ; num functions
+0000014: 00                                        ; function 0 signature index
+0000012: 02                                        ; FIXUP section size
+; section "Export" (7)
+0000015: 07                                        ; section code
+0000016: 00                                        ; section size (guess)
+0000017: 01                                        ; num exports
+0000018: 0d                                        ; string length
+0000019: 6164 6454 776f 506c 7573 5465 6e         addTwoPlusTen  ; export name
+0000026: 00                                        ; export kind
+0000027: 00                                        ; export func index
+0000016: 11                                        ; FIXUP section size
+; section "Code" (10)
+0000028: 0a                                        ; section code
+0000029: 00                                        ; section size (guess)
+000002a: 01                                        ; num functions
+; function body 0
+000002b: 00                                        ; func body size (guess)
+000002c: 02                                        ; local decl count
+000002d: 01                                        ; local type count
+000002e: 7f                                        ; i32
+000002f: 01                                        ; local type count
+0000030: 7e                                        ; i64
+0000031: 41                                        ; i32.const
+0000032: 0a                                        ; i32 literal
+0000033: 21                                        ; local.set
+0000034: 02                                        ; local index
+0000035: 42                                        ; i64.const
+0000036: cc21                                      ; i64 literal
+0000038: 21                                        ; local.set
+0000039: 03                                        ; local index
+000003a: 20                                        ; local.get
+000003b: 00                                        ; local index
+000003c: 20                                        ; local.get
+000003d: 01                                        ; local index
+000003e: 6a                                        ; i32.add
+000003f: 20                                        ; local.get
+0000040: 02                                        ; local index
+0000041: 6a                                        ; i32.add
+0000042: 0b                                        ; end
+000002b: 17                                        ; FIXUP func body size
+0000029: 19                                        ; FIXUP section size
+; section "name"
+0000043: 00                                        ; section code
+0000044: 00                                        ; section size (guess)
+0000045: 04                                        ; string length
+0000046: 6e61 6d65                                name  ; custom section name
+000004a: 02                                        ; local name type
+000004b: 00                                        ; subsection size (guess)
+000004c: 01                                        ; num functions
+000004d: 00                                        ; function index
+000004e: 00                                        ; num locals
+000004b: 03                                        ; FIXUP subsection size
+0000044: 0a                                        ; FIXUP section size
  */
